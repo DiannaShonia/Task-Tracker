21e9851ca0ce19bff962a770d7a9704e4153eae4
Lily <DianaShonia999@gmail.com>
2021-02-07 20:45:46 +0400

    added json server

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text,}) => {
+const Button = ({ color, text, onClick}) => {
     return (
-        <button className="btn">
+        <button 
+        onClick={ onClick}
+        style={{ backgroundColor: color}}
+        className="btn">
             {text}
         </button>
     )
